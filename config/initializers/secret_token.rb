221241286fa62954571e01667fe00f20e86efae4
Aron Hegyi <ahegyi@gmail.com>
2013-04-17 14:45:52 -0700

    First commit, omniauth-facebook + devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmniauthFacebookLab::Application.config.secret_token = 'a310a04a9ae1ba20b3d4a89d0ca9cea2bcc71839cd88c633cf3becd2ef6a98818def14c52745887079c5e46b45877c686b3b692a5eacd316c2ee64ef79f38337'
